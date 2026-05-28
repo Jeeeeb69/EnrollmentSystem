9dbc0fec181c2749d34c52772f1e70cab1a0094f
Jave Sumatra <javesumatra2@gmail.com>
2026-05-28 11:43:38 +0800

    fix database and static config

--- enrollment_system/settings.py
+++ enrollment_system/settings.py
@@ -139,16 +139,7 @@ elif all(
     os.environ.get(key)
     for key in ("PGDATABASE", "PGUSER", "PGPASSWORD", "PGHOST")
 ):
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ["PGDATABASE"],
-            "USER": os.environ["PGUSER"],
-            "PASSWORD": os.environ["PGPASSWORD"],
-            "HOST": os.environ["PGHOST"],
-            "PORT": os.environ.get("PGPORT", "5432"),
-        }
-    }
+
     DATABASE_SOURCE = "PGDATABASE/PGUSER/PGPASSWORD/PGHOST"
 elif all(
     os.environ.get(key)

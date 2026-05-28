de6a0d50a94adb7866890ea025623749b1013fa1
Jave Sumatra <javesumatra2@gmail.com>
2026-05-28 13:55:49 +0800

    Make email verified migration safe for deploy

--- core/migrations/0007_user_email_verified.py
+++ core/migrations/0007_user_email_verified.py
@@ -1,6 +1,23 @@
 from django.db import migrations, models
 
 
+def add_email_verified_if_missing(apps, schema_editor):
+    User = apps.get_model("core", "User")
+    table_name = User._meta.db_table
+    column_names = {
+        column.name
+        for column in schema_editor.connection.introspection.get_table_description(
+            schema_editor.connection.cursor(),
+            table_name,
+        )
+    }
+
+    if "email_verified" not in column_names:
+        field = models.BooleanField(default=False)
+        field.set_attributes_from_name("email_verified")
+        schema_editor.add_field(User, field)
+
+
 def mark_existing_active_users_verified(apps, schema_editor):
     User = apps.get_model("core", "User")
     User.objects.filter(is_active=True).update(email_verified=True)
@@ -13,10 +30,20 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name="user",
-            name="email_verified",
-            field=models.BooleanField(default=False),
+        migrations.SeparateDatabaseAndState(
+            database_operations=[
+                migrations.RunPython(
+                    add_email_verified_if_missing,
+                    migrations.RunPython.noop,
+                ),
+            ],
+            state_operations=[
+                migrations.AddField(
+                    model_name="user",
+                    name="email_verified",
+                    field=models.BooleanField(default=False),
+                ),
+            ],
         ),
         migrations.RunPython(
             mark_existing_active_users_verified,
